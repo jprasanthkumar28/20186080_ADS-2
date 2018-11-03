34a7e44e12608599556acc808b812036fa4bb6d7
jprasanthkumar28 <jprasanthkumar28@msitprogram.net>
2018-11-03 12:57:24 +0530

    "ads2_m6_a1_1: 0/12 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 1/1 "

--- Assignments/M6/ADS-2Week-1/Solution.java
+++ Assignments/M6/ADS-2Week-1/Solution.java
@@ -5,6 +5,7 @@ class PageRank {
 	 * digraph varible.
 	 */
 	private Digraph digraph;
+    private Bag<Integer>[] adj;
 	private int vertices;
 	private int edges;
 	PageRank(final Digraph digraph1) {
@@ -20,18 +21,28 @@ class PageRank {
 		return count;
 	}
 	/**
-     * to display the graph.
-     */
-    public String toString() {
-    	String str = "";
-        Digraph digraph = new Digraph(vertices);
-        if (digraph.outDegreeCount() == 0) {
-            digraph.addEdge(vertices, edges);
-        } else {
-        	str = vertices +"-" + getPR();
-            System.out.println(str);
+	 * to display the graph.
+	 */
+	public String display() {
+		String str = "";
+		Digraph digraph = new Digraph(vertices);
+		if (digraph.outDegreeCount() == 0) {
+			digraph.addEdge(vertices, edges);
         }
         return str;
+	}
+
+    public String toString() {
+        StringBuilder s = new StringBuilder();
+        s.append(vertices + " vertices, " + edges + " edges " + "\n");
+        for (int v = 0; v < vertices; v++) {
+            s.append(String.format("%d: ", v));
+            for (int w : adj[v]) {
+                s.append(String.format("%d ", w));
+            }
+            s.append("\n");
+        }
+        return s.toString();
     }
 }
 
@@ -44,25 +55,25 @@ public class Solution {
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
 		// read the first line of the input to get the number of vertices
-        int vertices = Integer.parseInt(scan.nextLine());
-        Digraph digraph = new Digraph(vertices);
+		int vertices = Integer.parseInt(scan.nextLine());
+		Digraph digraph = new Digraph(vertices);
 		// iterate count of vertices times 
 		// to read the adjacency list from std input
 		// and build the graph
-	    while (vertices > 0) {
-	        String[] tokens = scan.nextLine().split(" ");
-	        // System.out.println(Arrays.toString(tokens));
-	        int v = Integer.parseInt(tokens[0]);
-	        int w = Integer.parseInt(tokens[1]);
-	        digraph.addEdge(v, w);
-	        vertices--;
-        }
+		while (vertices > 0) {
+			String[] tokens = scan.nextLine().split(" ");
+			// System.out.println(Arrays.toString(tokens));
+			int v = Integer.parseInt(tokens[0]);
+			int w = Integer.parseInt(tokens[1]);
+			digraph.addEdge(v, w);
+			vertices--;
+		}
 		
 		// Create page rank object and pass the graph object to the constructor
 		PageRank p = new PageRank(digraph);
 		// print the page rank object
 		p.getPR();
-		p.toString();	
+		p.toString();
 		// This part is only for the final test case
 		
 		// File path to the web content
@@ -74,7 +85,6 @@ public class Solution {
 		// read the search queries from std in
 		// remove the q= prefix and extract the search word
 		// pass the word to iAmFeelingLucky method of web search
-		// print the return value of iAmFeelingLucky
-		
+		// print the return value of iAmFeelingLucky    
 	}
 }
