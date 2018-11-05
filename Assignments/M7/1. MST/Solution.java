d258467251daab29459cd1aaf54cb4bd9f5cd244
jprasanthkumar28 <jprasanthkumar28@msitprogram.net>
2018-11-05 18:42:21 +0530

    "ads2_m7_a1_1: 6/6 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- Assignments/M7/1. MST/Solution.java
+++ Assignments/M7/1. MST/Solution.java
@@ -3,31 +3,31 @@ import java.util.Scanner;
  * Class for solution.
  */
 public final class Solution {
-	/**
-	 * Constructs the object.
-	 */
-	private Solution() {
-		//Empty Constructer.
-	}
-	/**
-	 * Client function.
-	 *
-	 * @param      args  The arguments
-	 */
-	public static void main(final String[] args) {
-		Scanner scan = new Scanner(System.in);
-		int vertices = Integer.parseInt(scan.nextLine());
-		int edges = Integer.parseInt(scan.nextLine());
-		EdgeWeightedGraph edgeWeight
-		 = new EdgeWeightedGraph(vertices);
-		for (int i = 0; i < edges; i++) {
-			String[] tokens = scan.nextLine().split(" ");
-			Edge edge = new Edge(Integer.parseInt(tokens[0]),
-				Integer.parseInt(tokens[1]),
-				Double.parseDouble(tokens[2]));
-			edgeWeight.addEdge(edge);
-		}
-		KruskalMST krushkal = new KruskalMST(edgeWeight);
-		System.out.printf("%.5f\n", krushkal.weight());
-	}
+    /**
+     * Constructs the object.
+     */
+    private Solution() {
+        //Empty Constructer.
+    }
+    /**
+     * Client function.
+     *
+     * @param      args  The arguments
+     */
+    public static void main(final String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int vertices = Integer.parseInt(scan.nextLine());
+        int edges = Integer.parseInt(scan.nextLine());
+        EdgeWeightedGraph edgeWeight
+         = new EdgeWeightedGraph(vertices);
+        for (int i = 0; i < edges; i++) {
+            String[] tokens = scan.nextLine().split(" ");
+            Edge edge = new Edge(Integer.parseInt(tokens[0]),
+                Integer.parseInt(tokens[1]),
+                Double.parseDouble(tokens[2]));
+            edgeWeight.addEdge(edge);
+        }
+        KruskalMST krushkal = new KruskalMST(edgeWeight);
+        System.out.printf("%.5f\n", krushkal.weight());
+    }
 }
